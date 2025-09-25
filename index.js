566d420a0ef3ac8a650c94bdeaef538ac85bf54c
divyani <ikrakizoi2607@gmail.com>
2025-09-26 00:33:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import reactDom from "react-dom/client";
+import App from "./src/App"
+
+
+const root = reactDom.createRoot(document.getElementById("root"));
+
+root.render(<App />);
