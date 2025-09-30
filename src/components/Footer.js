123a92056cbf67ecb6c89d73309d19122f49519e
divyani <ikrakizoi2607@gmail.com>
2025-09-30 16:39:43 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../css/Footer.css'
+
+export default function Footer(){
+    return(
+        <div className="footer">
+            <p>
+                Made with ❤️ by <a href="">binbard</a>
+            </p>
+        </div>
+    )
+}
